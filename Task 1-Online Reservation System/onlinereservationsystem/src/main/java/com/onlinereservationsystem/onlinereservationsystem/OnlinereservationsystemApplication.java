07059c9de3a4e5d97302b33c79c12fbe6d82c5ad
AdityaJoshiK <adityajoshi2107@gmail.com>
2023-06-11 19:49:16 +0530

    Initialized

---
+++ Task 1-Online Reservation System/onlinereservationsystem/src/main/java/com/onlinereservationsystem/onlinereservationsystem/OnlinereservationsystemApplication.java
@@ -0,0 +1,13 @@
+package com.onlinereservationsystem.onlinereservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinereservationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinereservationsystemApplication.class, args);
+	}
+
+}
---
+++ Task 1-Online Reservation System/onlinereservationsystem/src/test/java/com/onlinereservationsystem/onlinereservationsystem/OnlinereservationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinereservationsystem.onlinereservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinereservationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
